10454fc84164c641a69e6dcda7e9a57f2771a64d
Sanne de Roever <sanne.de.roever@gmail.com>
2025-10-13 11:58:49 +0200

    Added https://taskfile.dev example.

--- robot-test.sh
+++ robot-test.sh
@@ -3,6 +3,6 @@ robot reason \
   --create-new-ontology true \
   --equivalent-classes-allowed all\
   --axiom-generators "SubClass EquivalentClass DisjointClasses ClassAssertion PropertyAssertion" \
-  --output out.ttl \
+  --output output/toy-inferred.ttl \
   --include-indirect true \
   --reasoner hermit
